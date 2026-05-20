327335fec11f4e0341b1eda9ba0b172f22397d29
LeChacal <dev.lechacal@gmail.com>
2026-05-20 13:44:30 +0200

    Builded

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,20 @@
-import {defineConfig} from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-    plugins: [react()],
+export default defineConfig( {
+    plugins: [
+        react(),
+        tailwindcss()
+    ],
     build: {
         outDir: '../static',
         emptyOutDir: true
+    },
+    server: {
+        proxy: {
+            '/max-filesize': 'https://share.oliveri.dev',
+            '/upload': 'https://share.oliveri.dev',
+        }
     }
-})
+} )
